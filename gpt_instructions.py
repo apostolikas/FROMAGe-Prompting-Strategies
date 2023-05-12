3d0401cba128cc7365569c302b908c6910341f6e
vasilisvyth <72560545+vasilisvyth@users.noreply.github.com>
2023-05-12 16:25:11 +0200

    few changes in gpt_instructions

--- gpt_instructions.py
+++ gpt_instructions.py
@@ -1,39 +1,23 @@
 import openai
+import time
 
-openai_api_key = "your key"
-gpt_version = ""
-def prompt_llm(prompt, max_tokens=64, temperature=0, stop=None):
+openai.api_key = "your key"
+gpt_version = "text-davinci-002"
+def prompt_llm(prompt, max_tokens = 64, temperature=0, stop=None):
   response = openai.Completion.create(engine=gpt_version, prompt=prompt, max_tokens=max_tokens, temperature=temperature, stop=stop)
   return response["choices"][0]["text"].strip()
 
-classes=['electric guitar',			
-'golden retriever',
-'malamute',
-'mixing bowl',
-'cuirass',
-'dalmatian',
-'african hunting dog',
-'lion',
-'crate',
-'bookshop',
-'vase',
-'nematode',
-'hourglass',
-'ant',
-'king crab',
-'black-footed ferret',
-'scoreboard',
-'theater curtain',
-'school bus',
-'trifle']
+captions = ['electric guitar']		
 
 instructions_dict = {}
-for label_class in classes:
-    prompt_list = [f'Q: What are useful features for distinguishing a {label_class} in a photo?\
-                    A: There are several useful visual features to tell there is a {label_class} in a photo:',
-                    f'What is a {label_class}']
-    instructions_dict[label_class] =[]
+max_generated_tokens  = 10
+for i,label_class in enumerate(captions):
+    prompt_list = [#f'Q: What are useful features for distinguishing a {label_class} in a photo? A: There are several useful visual features to tell there is a {label_class} in a photo:',
+                    f'Give more details for the following text: {label_class}']
+    instructions_dict[label_class] = []
     for prompt in prompt_list:
-       #! maybe increase temperature for more diversity
-       ans = prompt_llm(prompt,max_tokens=64, temperature=0)
+       #! maybe increase temperature for more diversity but more diversity may give us more bad words
+       ans = prompt_llm(prompt, max_tokens = max_generated_tokens, temperature = 0)
        instructions_dict[label_class].append(ans)
+    #! openai doesn't like very frequent requests so maybe we need to use some sleep function after some time
+    time.sleep(5) #seconds
