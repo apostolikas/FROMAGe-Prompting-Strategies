dbc79c73704ffc149930eb02023b9b8a69bd1bca
Nick <n.apostolikas@hotmail.com>
2023-05-13 17:58:49 +0200

    task update

--- augmented_text_img_retr.py
+++ augmented_text_img_retr.py
@@ -8,6 +8,9 @@ from torchvision.transforms import ToTensor
 from torchmetrics.multimodal import CLIPScore
 
 
+extended_captions = open("extended_captions.txt", "r")
+augmented_captions = [x.rstrip("\n") for x in extended_captions.readlines()]
+
 #! Load and preprocess data
 df = pd.read_csv('./Flickr8k_text/ExpertAnnotations.txt',delimiter='\t')
 cropped_df = df.loc[df['expert1'] == 4]
@@ -19,6 +22,8 @@ for img_id, cap_id in zip(cropped_df.image_id, cropped_df.caption_id):
     caption = cap_dict[cap_id]
     data_dict[img_id] = caption 
 
+for i,content in enumerate(zip(data_dict.keys(),data_dict.values())):
+    data_dict[content[0]] = (content[1],augmented_captions[i])
 
 #! function to make a dictionary -> list of dictionaries (len(list)=chunk_size) (in our case 1)
 def split_dictionary(input_dict, chunk_size):
@@ -44,39 +49,51 @@ ic_data = ic_data[:num_of_samples]
 model_dir = './fromage_model/'
 model = models.load_fromage(model_dir)
 
+
+#! function to make a dictionary -> list of dictionaries (len(list)=chunk_size) (in our case 1)
+def split_dictionary(input_dict, chunk_size):
+    res = []
+    new_dict = {}
+    for k, v in input_dict.items():
+        if len(new_dict) < chunk_size:
+            new_dict[k] = v
+        else:
+            res.append(new_dict)
+            new_dict = {k: v}
+    res.append(new_dict)
+    return res
+
+#! final data for task
+ic_data = split_dictionary(data_dict,1)
+ic_data = ic_data[:2]
+
+#! load model
+model_dir = './fromage_model/'
+model = models.load_fromage(model_dir)
+
 i=0 # counter for print 
 metric = CLIPScore(model_name_or_path="openai/clip-vit-base-patch16")
-#images_dict = {}
 scores = []
 
 for ic_dict in ic_data:
+
     i+=1
     image_path = list(ic_dict.keys())[0]
     image = Image \
         .open(os.path.join('./Flicker8k_Dataset/',image_path)) \
         .resize((224, 224)) \
-        .convert('RGB')
+        .convert('RGB') 
     caption = list(ic_dict.values())[0]
 
-    #! First check if the model retrieves a good image based on the caption
-    prompt = [caption[:-1] + ' [RET] ']
-    retrieved_image = model.generate_for_images_and_texts(prompt, max_img_per_ret=1, max_num_rets=1)
+    # Retrieve image based on the augmented caption
+    augmented_caption = list(ic_dict.values())[1]
+    augmented_prompt = [augmented_caption[:-1] + ' [RET] ']
+    model_output = model.generate_for_images_and_texts(augmented_prompt, max_img_per_ret=1, max_num_rets=1)
+    retrieved_image = model_output[0]
+    retrieved_image.save(str(i)+'ret_img.jpg')
 
-    # Optional - Qualitative check on retrieved_image
-    # retrieved_image[0].save(str(i) + 'retrieved_image.jpg')
-
-    #TODO Augment caption 
-
-    new_prompt = [augmented_caption[:-1] + ' [RET] ']
-    model_output = model.generate_for_images_and_texts(prompt, max_img_per_ret=1, max_num_rets=1)
-    new_retrieved_image = model_output[0]
-    # Optional - Qualitative check on new retrieved_image
-    # new_retrieved_image.save(str(i) + 'new_retrieved_image.jpg')
-
-    #images_dict[caption] = zip(image, new_retrieved_image)
-
-    #TODO ClipScore 0-100%
+    # Compare augmented caption with original and retrieved image
     transform = ToTensor()
-    score_with_original_image = metric(transform(image), caption)
-    score_with_new_image = metric(transform(new_retrieved_image), caption)
+    score_with_original_image = metric(transform(image), augmented_caption)
+    score_with_new_image = metric(transform(retrieved_image), augmented_caption)
     print("Example ", i, "\t Original img score :", score_with_original_image.detach().item,"%", "\t New img score :", score_with_new_image.detatch().item,"%")
