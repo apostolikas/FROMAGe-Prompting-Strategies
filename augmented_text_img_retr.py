500125500c07bed7f06ba0104f5fc94a71d9247f
Nick <n.apostolikas@hotmail.com>
2023-05-14 22:06:42 +0200

    script fixed + img-img comparison

--- augmented_text_img_retr.py
+++ augmented_text_img_retr.py
@@ -5,7 +5,7 @@ import numpy as np
 from fromage import models
 import matplotlib as plt
 from torchvision.transforms import ToTensor
-from torchmetrics.multimodal import CLIPScore
+import torch
 
 
 extended_captions = open("extended_captions.txt", "r")
@@ -40,18 +40,14 @@ def split_dictionary(input_dict, chunk_size):
 
 #! final data for task
 ic_data = split_dictionary(data_dict,1)
-#ic_data = ic_data[:50]
 
 #! load model
 model_dir = './fromage_model/'
 model = models.load_fromage(model_dir)
 
 i=0 # counter for print 
-metric = CLIPScore(model_name_or_path="openai/clip-vit-base-patch16")
-scores_orig_img_orig_cap = []
-scores_ret_img_orig_cap = []
-scores_orig_img_aug_cap = []
-scores_ret_img_aug_cap = []
+unaugmented_scores = []
+augmented_scores = []
 
 for ic_dict in ic_data:
 
@@ -63,42 +59,38 @@ for ic_dict in ic_data:
         .convert('RGB') 
     caption_tuple = list(ic_dict.values())[0]
 
-    # Retrieve image based on the augmented caption
+    # Retrieve image based on the original caption
     original_caption = caption_tuple[0]
     original_prompt = [original_caption[:-1] + ' [RET] ']
     model_output_orig = model.generate_for_images_and_texts(original_prompt, max_img_per_ret=1, max_num_rets=1, num_words=0)
-    orig_img = model_output_orig[-1][0]
-    #orig_img.save(str(i)+'_orig_img.jpg')
+    unaugmented_output = model_output_orig[-1][0]
+    #unaugmented_output.save(str(i)+'_orig_img.jpg')
 
+    # Retrieve image based on the augmented caption
     augmented_caption = caption_tuple[1]
-    augmented_prompt = [augmented_caption[:-1] + ' [RET] ']
+    augmented_prompt = [image, augmented_caption[:-1] + ' [RET] ']
     model_output = model.generate_for_images_and_texts(augmented_prompt, max_img_per_ret=1, max_num_rets=1, num_words=0)
-    retrieved_image = model_output[-1][0]
+    augmented_output = model_output[-1][0]
     #retrieved_image.save(str(i)+'_ret_img.jpg')
-
 
     # Compare augmented caption with original and retrieved image
     transform = ToTensor()
-    score_orig_cap_orig_img = metric(transform(image), original_caption)
-    score_aug_cap_orig_img = metric(transform(image), augmented_caption)
-    score_ret_img_orig_cap = metric(transform(retrieved_image), original_caption)
-    score_ret_img_augm_cap = metric(transform(retrieved_image), augmented_caption)
-
-    scores_orig_img_orig_cap.append(score_orig_cap_orig_img.detach().item())
-    scores_ret_img_aug_cap.append(score_ret_img_augm_cap.detach().item())
-    scores_orig_img_aug_cap.append(score_aug_cap_orig_img.detach().item())
-    scores_ret_img_orig_cap.append(score_ret_img_orig_cap.detach().item())
-
-
+    model = model.to(torch.float16)
+    with torch.no_grad():
+        embedding_augmented = model.model.get_visual_embs((transform(augmented_output)).unsqueeze(0).half().cuda())
+        embedding_unaugmented = model.model.get_visual_embs((transform(unaugmented_output)).unsqueeze(0).half().cuda())
+        embedding_original = model.model.get_visual_embs((transform(image)).unsqueeze(0).half().cuda())
+    model = model.bfloat16()
+    similarity_score_unaugmented = torch.nn.functional.cosine_similarity(embedding_unaugmented.float(), embedding_original.float())
+    similarity_score_augmented = torch.nn.functional.cosine_similarity(embedding_augmented.float(), embedding_original.float())
+    augmented_scores.append(similarity_score_augmented.mean(1).item())
+    unaugmented_scores.append(similarity_score_unaugmented.mean(1).item())
+    
     print("Example ", i)
-    print("Original Caption  - Original img score :", score_orig_cap_orig_img.detach().item() ,"%")
-    print("Original Caption  - Retrieved img score :", score_ret_img_orig_cap.detach().item() ,"%")
-    print("Augmented Caption - Original img score :", score_aug_cap_orig_img.detach().item() ,"%")
-    print("Augmented Caption - Retrieved img score :", score_ret_img_augm_cap.detach().item() ,"%")
+    print("Similarity of Unaugmented - Original ",similarity_score_unaugmented.mean(1).item())
+    print("Similarity of Augmented - Original ",similarity_score_augmented.mean(1).item())
     print("------------------------------------------------")
 
 print("\nIn total:")
-print("The average original caption - original img score is ", np.mean(scores_orig_img_orig_cap),"%")
-print("The average original caption - retrieved img score is ", np.mean(scores_ret_img_orig_cap),"%")
-print("The average augmented caption - original img score is ", np.mean(scores_orig_img_aug_cap),"%")
-print("The average augmented caption - retrieved img score is ", np.mean(scores_ret_img_aug_cap),"%")
+print("Using the original caption, the average cosine similarity with the target image is ", np.mean(unaugmented_scores))
+print("Using the augmented caption, the average cosine similarity with the target image is ", np.mean(augmented_scores))
