45ecacc788fd38fa683cae830ded5f96f9a39806
Denny Smit <dennysmit@hotmail.com>
2023-05-22 15:50:36 +0200

    Made ready for annotation

--- dialog_in_context.py
+++ dialog_in_context.py
@@ -8,8 +8,9 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("--num_tests", type=int, required=True)
 parser.add_argument("--num_prompts_per_test", type=int, required=True)
-# parser.add_argument("--ret_img", type=bool, required=True)
 parser.add_argument('--ret_img', type=lambda x: (str(x).lower() == 'true'), default=False)
+parser.add_argument('--augment_prompt', type=lambda x: (str(x).lower() == 'true'), default=False)
+parser.add_argument('--include_Q_A', type=lambda x: (str(x).lower() == 'true'), default=False)
 parser.add_argument("--num_qa_per_dialog", type=int, required=True)
 
 args = parser.parse_args()
@@ -18,6 +19,8 @@ num_tests = args.num_tests
 num_pt = args.num_prompts_per_test
 ret_img = args.ret_img
 num_qa_per_dialog = args.num_qa_per_dialog
+adapt_prompt_gpt = args.augment_prompt
+include_Q_A = args.include_Q_A
 
 print(f"number of experiments = {num_tests}, number of dailogs per test = {num_pt}, number of qa's per dialog = {num_qa_per_dialog}, return image is {ret_img}")
 
@@ -30,11 +33,11 @@ dialogs_csv_path = 'visual_dialog/dialogs.csv'
 dialogs_df = dialog_utils.load_dialogs(dialogs_csv_path)
 
 # Obtain a certain amount of dialogs (more than needed to be able to handle errors)
-num_rows = 10000
+num_rows = 800
 if num_rows > len(dialogs_df):
     num_rows = len(dialogs_df)
 
-dialog_list, url_dialog_list = dialog_utils.get_prompt_list(dialogs_df, num_rows, num_qa_per_dialog, ret_img)
+dialog_list, url_dialog_list = dialog_utils.get_prompt_list(dialogs_df, num_rows, num_qa_per_dialog, ret_img, adapt_prompt_gpt, include_Q_A)
 
 prompt, prompt_to_save = [], []
 prompt_list, model_outputs = [], []
@@ -77,7 +80,7 @@ for i in range(len(dialog_list)):
 
         # Make sure that invalid output will be skipped
         try:
-            image_outputs, output_urls_or_caption = model.generate_for_images_and_texts(prompt, max_img_per_ret=3, max_num_rets=max_num_rets, num_words=num_words)
+            image_outputs, output_urls_or_caption = model.generate_for_images_and_texts(list(prompt), max_img_per_ret=3, max_num_rets=max_num_rets, num_words=num_words)
             # Add the prompts with the image id to the prompt list and the output containing urls to the model outputs
             print("@@@@@@@@@@@@@@@@@@@")
             print(image_outputs)
@@ -109,4 +112,4 @@ for i in range(len(dialog_list)):
             prompt_to_save += url_dialog_list[i]
             prompt += dialog_list[i]
 
-dialog_utils.save_dialogs(prompt_list, model_outputs, num_pt, num_qa_per_dialog, ret_img, provide_context)
+dialog_utils.save_dialogs(prompt_list, model_outputs, num_pt, num_qa_per_dialog, ret_img, provide_context, include_Q_A, adapt_prompt_gpt)
--- visual_dialog/dialog_utils.py
+++ visual_dialog/dialog_utils.py
@@ -8,9 +8,16 @@ from PIL import Image
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 from fromage.utils import get_image_from_url
+import openai
 
-images_path = 'visual_dialog/VisualDialog_val2018/'""
+from dotenv import load_dotenv
+
+# # Load the environment variables from .env
+load_dotenv()
+
+images_path = 'visual_dialog/VisualDialog_val2018/'
 dialogs_csv_path = 'dialogs.csv'
+openai.api_key = os.getenv("OPENAI_KEY")
 
 # CREATE DIALOG DATAFRAME
 
@@ -27,7 +34,6 @@ def create_stories(dialogs_path:str) -> pd.DataFrame:
         id = i
 
         image_id = dialogs['data']['dialogs'][i]['image_id']
-        # get_image(image_id)
 
         for j in range(len(dialogs['data']['dialogs'][i]['dialog'])):
                 round = j+1
@@ -77,23 +83,49 @@ def show_image(image):
     plt.imshow(np.array(image))
     plt.show()
 
-        
+instruction = """Transform the following caption 
+with a question and answer dialogue about an image 
+into a caption as short as possible while capturing 
+all the information that is given: """
+
+def gpt_prompt(prompt):
+    input_prompt = instruction + prompt
+    print("INPUT PROMPT TO GPT")
+    print(input_prompt)
+    # Generate text with a maximum length of 100 tokens
+    response = openai.Completion.create(
+        engine='text-davinci-003',
+        prompt= input_prompt,
+        temperature=0,
+        max_tokens=100,
+        n=1,
+        stop=None,
+    )
+
+    adapted_prompt = response.choices[0].text.strip()
+    return adapted_prompt
+
 # Create prompts from dataframe
-def get_prompt_list(dialogs_df, num_rows, prompt_length, ret_img=True):
+def get_prompt_list(dialogs_df, num_rows, prompt_length, ret_img=True, adapt_gpt_prompt=True, include_Q_A=False):
     text = ""
     dialog, url_dialog, input_dialog_list, url_dialog_list = [], [], [], []
     for i in range(num_rows-1):
         if int(dialogs_df['round'][i]) == 1:
             image_id = dialogs_df['image_id'][i]
             caption = dialogs_df['caption'][i]
-            text += caption
+            text += f"Caption: {caption}. "
             img = get_image(image_id)
             # dialog.append(img)
         if int(dialogs_df['round'][i]) <= prompt_length:
-            text += f"Q: {dialogs_df['question'][i]}, "
-            text += f"A: {dialogs_df['answer'][i]}, "
+            if include_Q_A == True:
+                text += f"Q: {dialogs_df['question'][i]}, "
+                text += f"A: {dialogs_df['answer'][i]}, "
         if dialogs_df['id'][i+1] != dialogs_df['id'][i]:
             text = text[:-2]
+            if adapt_gpt_prompt == True:
+                text = gpt_prompt(text)
+                print("ADAPTED GPT PROMPT")
+                print(text)
             if ret_img == True: 
                 dialog.append(text)
                 dialog.append(img)
@@ -113,8 +145,9 @@ def get_prompt_list(dialogs_df, num_rows, prompt_length, ret_img=True):
 
     # capture the last row
     if prompt_length == 10:
-        text += f"Q: {dialogs_df['question'][num_rows]}, "
-        text += f"A: {dialogs_df['answer'][num_rows]}"
+        if include_Q_A == True:
+            text += f"Q: {dialogs_df['question'][num_rows]}, "
+            text += f"A: {dialogs_df['answer'][num_rows]}"
     if ret_img == True: 
         url_dialog.append(text)
         url_dialog.append(image_id)
@@ -137,7 +170,16 @@ def display_prompt(output_list):
         try:
             get_image(output, show_image=True)
         except:
-            print(output)
+            split_Q = output.split('Q')
+            for i, line in enumerate(split_Q):
+                if len(split_Q) > 1:
+                    if i > 0:
+                        print(f'Q{line}')
+                    else:
+                        print(line)
+                else:
+                    print(line)
+
 
 # Display the output of the model, retrieve the images by their url
 def display_output(story_list):
@@ -150,14 +192,24 @@ def display_output(story_list):
                 plt.imshow(np.array(image))
                 plt.show()
             except:
-                print(element)
+                split_Q = element.split('Q')
+                for i, line in enumerate(split_Q):
+                    if len(split_Q) > 1:
+                        if i > 0:
+                            print(f'Q{line}')
+                        else:
+                            print(line)
+                    else:
+                        print(line)
 
 # Save prompts and output into file with name corresponding to experiment
-def save_dialogs(prompts, outputs, num_examples, num_qa, ret_img, provide_context):
-    if provide_context == True:
-        path = f"visual_dialog/{num_examples}_examples_{num_qa}_qa_{'ret_img' if ret_img==True else 'ret_text'}.json"
-    else:
-        path = f"visual_dialog/{num_examples}_examples_{num_qa}_qa_{'ret_img' if ret_img==True else 'ret_text'}_nocontext.json"
+def save_dialogs(prompts, outputs, num_examples, num_qa, ret_img, provide_context, include_Q_A, adapt_prompt_gpt):
+    # if provide_context == True:
+        # path = f"visual_dialog/{num_examples}_examples_{num_qa}_qa_{'ret_img' if ret_img==True else 'ret_text'}.json"
+    path = f"visual_dialog/{num_examples}_examples_{num_qa}_qa_{'inc_QA' if include_Q_A==True else 'only_caption'}{'_GPT' if adapt_prompt_gpt==True else ''}.json"
+    # else:
+    #     path = f"visual_dialog/{num_examples}_examples_{num_qa}_qa_{'ret_img' if ret_img==True else 'ret_text'}_nocontext.json"
+
     data = {
         "prompts": prompts,
         "outputs": outputs
